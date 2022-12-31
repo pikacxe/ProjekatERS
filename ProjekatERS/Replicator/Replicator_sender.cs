26204e8598a40a700b901c0311675ab1e2d88002
Roland <deak.pr22.2020@uns.ac.rs>
2022-12-31 15:12:19 +0100

    created Replicator_sender

---
+++ ProjekatERS/Replicator/Replicator_sender.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Replicator
+{
+    public class Replicator_sender
+    {
+
+    }
+}
+
+
+/*Replicator Sender:
+        -   Komunikacija sa Writer-om
+        -   prima podatke od writer-a
+        -   prosledjuje podatke Replikator reciever   
+        -   Testiranje*/
