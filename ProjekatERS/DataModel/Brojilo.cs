bef7ac02eca2ce23e836d077410ae54a558731a4
bbratic <bojana.bratic14@gmail.com>
2023-01-05 16:31:48 +0100

    implemented Writer

--- ProjekatERS/DataModel/Brojilo.cs
+++ ProjekatERS/DataModel/Brojilo.cs
@@ -8,23 +8,69 @@ namespace DataModel
 {
     public class Brojilo
     {
-        private int ID;
-        private string ImeKorisnika;
-        private string PrezimeKorisnika;
-        private string Ulica;
-        private int Broj;
-        private int PostanskiBroj;
-        private string Grad;
+        private int _ID;
+        private string _ImeKorisnika;
+        private string _PrezimeKorisnika;
+        private string _Ulica;
+        private int _Broj;
+        private int _PostanskiBroj;
+        private string _Grad;
 
         public Brojilo(int iD, string imeKorisnika, string prezimeKorisnika, string ulica, int broj, int postanskiBroj, string grad)
         {
-            ID = iD;
-            ImeKorisnika = imeKorisnika ?? throw new ArgumentNullException(nameof(imeKorisnika));
-            PrezimeKorisnika = prezimeKorisnika ?? throw new ArgumentNullException(nameof(prezimeKorisnika));
-            Ulica = ulica ?? throw new ArgumentNullException(nameof(ulica));
-            Broj = broj;
-            PostanskiBroj = postanskiBroj;
-            Grad = grad ?? throw new ArgumentNullException(nameof(grad));
+            _ID = iD;
+            _ImeKorisnika = imeKorisnika ?? throw new ArgumentNullException(nameof(imeKorisnika));
+            _PrezimeKorisnika = prezimeKorisnika ?? throw new ArgumentNullException(nameof(prezimeKorisnika));
+            _Ulica = ulica ?? throw new ArgumentNullException(nameof(ulica));
+            _Broj = broj;
+            _PostanskiBroj = postanskiBroj;
+            _Grad = grad ?? throw new ArgumentNullException(nameof(grad));
+        }
+
+        public int ID {
+        get { return _ID; } 
+        set { _ID = value; }
+        }
+
+        public string ImeKorisnika
+        {
+            get { return _ImeKorisnika; }
+            set { ImeKorisnika = value; }
+        }
+
+        public string PrezimeKorisnika
+        {
+            get { return _PrezimeKorisnika; }
+            set { PrezimeKorisnika = value; }
+        }
+
+        public string Ulica
+        {
+            get { return _Ulica; }
+            set { Ulica = value; }
+        }
+
+        public int Broj
+        {
+            get { return _Broj; }
+            set { _Broj = value; }
+        }
+
+        public int PostanskiBroj
+        {
+            get { return _PostanskiBroj; }
+            set { _PostanskiBroj = value; }
+        }
+
+        public string Grad
+        {
+            get { return _Grad; }
+            set { Grad = value; }
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
         }
     }
 }
--- ProjekatERS/DataModel/Potrosnja.cs
+++ ProjekatERS/DataModel/Potrosnja.cs
@@ -8,7 +8,53 @@ namespace DataModel
 {
     public class Potrosnja
     {
+        private int _IDB;
+        private double _Potrosnjab;
+        private int _Mesec;
 
+        public Potrosnja(int iDB, double potrosnjab, int mesec)
+        {
+            _IDB = iDB;
+            _Potrosnjab = potrosnjab;
+            _Mesec = mesec;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+
+        public int IDB
+        {
+            get { return _IDB; }
+            set { _IDB = value; }
+        }
+
+
+        public double PotrosnjaB
+        {
+            get
+            {
+                return _Potrosnjab;
+            }
+            set
+            {
+                _Potrosnjab = value;
+            }
+        }
+
+        public int Mesec
+        {
+            get
+            {
+                return _Mesec;
+            }
+            set
+            {
+                _Mesec = value;
+            }
+        }
 
     }
 }
