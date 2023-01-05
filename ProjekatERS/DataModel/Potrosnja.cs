d3d744a18940ebf73f7bf005b2233c055f499bb4
VladaPR26 <simicvla01@gmail.com>
2023-01-05 16:31:45 +0100

    Impleneted Replicator Receiver

--- ProjekatERS/DataModel/Potrosnja.cs
+++ ProjekatERS/DataModel/Potrosnja.cs
@@ -8,7 +8,54 @@ namespace DataModel
 {
     public class Potrosnja
     {
+        private int _IDB;
+        private double _PotrosnjaB;
+        private int _Mesec;
+
+        public int IDB { get 
+            {
+                return _IDB;    
+            }
+            set 
+            {
+                _IDB= value;
+            }
+        }
+
+        public double PotrosnjaB
+        {
+            get
+            {
+                return _PotrosnjaB;
+            }
+            set
+            {
+                _PotrosnjaB = value;
+            }
+        }
+        public int Mesec
+        {
+            get
+            {
+                return _Mesec;
+            }
+            set
+            {
+                _Mesec = value;
+            }
+        }
 
 
+        public Potrosnja(int iDB, double potrosnjaB, int mesec)
+        {
+            _IDB = iDB;
+            _PotrosnjaB = potrosnjaB;
+            _Mesec = mesec;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
     }
 }
