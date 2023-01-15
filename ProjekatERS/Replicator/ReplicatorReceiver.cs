047a8fcdfbbfa0e81b61db6e64936ce875dd7680
VladaPR26 <simicvla01@gmail.com>
2023-01-15 22:22:16 +0100

    Implementiran replicator reciever

--- ProjekatERS/Replicator/ReplicatorReceiver.cs
+++ ProjekatERS/Replicator/ReplicatorReceiver.cs
@@ -2,31 +2,71 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
+using Reader;
 
 namespace Replicator
 {
     public class ReplicatorReceiver
     {
 
-       private List<Potrosnja> potrosnje=new List<Potrosnja>();
-
+        private List<Potrosnja> potrosnje;
+        private Reader.Reader[] readers;
+        private int period;
+        private DateTime staroVreme;
         public ReplicatorReceiver()
         {
-
+            potrosnje = new List<Potrosnja>();
+            readers = new Reader.Reader[10];
         }
 
-        public void GetPotrosnja(Potrosnja potrosnja) 
+        public void GetPotrosnja(Potrosnja potrosnja)
         {
-            SavePotrosnja(potrosnja);
-        
+            try
+            {
+                if (potrosnja == null)
+                {
+                    throw new ArgumentNullException("Potrosnja ne sme biti null");
+                }
+                potrosnje.Add(potrosnja);
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
 
-        public void SavePotrosnja(Potrosnja potrosnja) 
+        public void SavePotrosnja()
         {
-            potrosnje.Add(potrosnja);    
+            try
+            {
+                if (potrosnje[1] == null)
+                {
+                    throw new ArgumentNullException("Potrosnja ne sme biti null");
+                }
+                Random random = new Random();
+                int i = random.Next(0, 10);
+                readers[i].SavePotrosnja(potrosnje[1]); //TODO srediti data setove 
+
+
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
+
+        private void MeriVreme()
+        {
+            if (DateTime.Now.Second >= staroVreme.Second)
+            {
+                SavePotrosnja();
+                staroVreme = DateTime.Now;
+                staroVreme.AddSeconds(period);
+            }
+        }
+
     }
 }
--- ProjekatERS/UIInterface/Program.cs
+++ ProjekatERS/UIInterface/Program.cs
@@ -3,13 +3,61 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Writer;
 
 namespace UIInterface
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private Writer.Writer[] writers=new Writer.Writer[10];
+        public static void Main(string[] args)
         {
+            string unos;
+            do
+            {
+                Console.WriteLine("1 - Unos trenutne potrosnje vode korisnika");
+                Console.WriteLine("2 - Izvestaj o potrosnji po mesecima za odredjenu ulicu");
+                Console.WriteLine("3 - Izvestaj o potrosnji po mesecima za konkretno brojilo");
+                Console.WriteLine("4 - Upali writer");
+                Console.WriteLine("5 - Ugasi writer");
+                Console.WriteLine("X - Izlazak iz programa");
+
+
+                unos = Console.ReadLine();
+
+                switch (unos)
+                {
+                    case "1":
+                        UnosPotrosnje();
+                        break;
+                    case "2":
+                        IzvestajUlica();
+                        break;
+                    case "3":
+                        IzvestajBrojilo();
+                        break;
+                    case"4":
+                        UpaliWriter();
+                        break;
+                    case "5":
+                        UgasiWriter();
+                        break;
+
+                }
+            } while (unos.ToUpper().Equals("X"));
         }
+        public static void UnosPotrosnje()
+        { 
+        
+        }
+        public static void IzvestajUlica()
+        { }
+        public static void IzvestajBrojilo()
+        { }
+        public static void UpaliWriter() 
+        { }
+        public static void UgasiWriter()
+        { }
+
     }
 }
