2d9fb732803c6d437c3fb8105022eb2186ec1627
Ilija Spasic <spasicilija1@gmail.com>
2023-01-15 22:41:31 +0100

    added update query

--- ProjekatERS/Reader/Reader.cs
+++ ProjekatERS/Reader/Reader.cs
@@ -42,22 +42,36 @@ namespace Reader
 
 
         }
+
+        private int ExistsById(int id, SqlConnection conn)
+        {
+            string query = "select count(id) from Potrosnja where IDB = @id";
+            using (SqlCommand comm = conn.CreateCommand())
+            {
+                comm.CommandText = query;
+                comm.Parameters.AddWithValue("id", id);
+                return (int)comm.ExecuteScalar();
+            }
+        }
         public void SavePotrosnja(Potrosnja potrosnja)
         {
             // TO-DO save potrosnja to DB
-            string query = "insert into Potrosnja(IDB,Potrosnja,Mesec) values (@id, @potrosnja, @mesec)";
+            string inser_query = "insert into Potrosnja(Potrosnja,Mesec,IDB) values ( @potrosnja, @mesec, @id)";
+            string update_query = "update Potrosnja set Potrosnja=@potrosnja, Mesec=@mesec where IDB = @id";
             using (SqlConnection conn = ConnectionParameters.GetConnection())
             {
                 conn.Open();
                 using (SqlCommand comm = conn.CreateCommand())
                 {
-                    comm.CommandText = query;
-                    comm.Parameters.AddWithValue("id", potrosnja.IDB);
+                    comm.CommandText = ExistsById(potrosnja.IDB, conn) == 1 ? update_query : inser_query;
                     comm.Parameters.AddWithValue("potrosnja", potrosnja.PotrosnjaB);
                     comm.Parameters.AddWithValue("mesec", potrosnja.Mesec);
+                    comm.Parameters.AddWithValue("id", potrosnja.IDB);
                     comm.ExecuteNonQuery();
                 }
             }
         }
+
+        // TO-DO Reports
     }
 }
